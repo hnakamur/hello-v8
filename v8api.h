2f37cd825996bdac1b3a782cd08bcf7e66d23dd1
Hiroaki Nakamura <hnakamur@gmail.com>
2022-05-05 23:54:05 +0900

    Also no coredump

--- v8api.h
+++ v8api.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <v8.h>
+#include "include/libplatform/libplatform.h"
 
 namespace v8_api
 {
     class Core
     {
     private:
+        std::unique_ptr<v8::Platform> platform_;
         v8::Isolate* isolate_;
         v8::Global<v8::Context> impl_;
 
